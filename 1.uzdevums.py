7c479eb78907db6140dda2b0b7a69fb71b3ec73f
bigtats <kruminskaspars4@gmail.com>
2023-11-24 14:38:22 +0200

    1uzdevums izpildits

--- 1.uzdevums.py
+++ 1.uzdevums.py
@@ -1,1 +1,8 @@
+#Izveidojiet Python programmu, kas saskaita no 1 līdz lietotāja ievadītam skaitlim, izmantojot for ciklu.
+sum=0
+a=int(input("ievadi skaitli.."))
+c=a+1
+for i in range(1,c,1):
+    suma=sum+1
+    print(suma)
 
